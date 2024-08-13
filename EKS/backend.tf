688cc3c13b2c1eaaf3991f6c53bcbb5e5420d841
manojacharya3 <manojkumar.nandachar@perficient.com>
2024-08-13 16:49:17 +0530

    files are modified

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "test-bucket-manoj-123"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
-# Java installation for jenkins
+# For Ubuntu 22.04
+# Intsalling Java
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+sudo apt install openjdk-17-jdk -y
+java --version
 
-sudo apt update
-sudo apt install openjdk-11-jre -y
-
-# Jenkins installation 
+# Installing Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install jenkins -y
 
-# Terraform Installation
-
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
+# Installing Terraform
+#!/bin/bash
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
-sudo apt-get install terraform -y
+sudo apt install terraform -y
+
 
 # Installing kubernetes
 
@@ -35,8 +30,9 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl 
 
-# Install AWS CLI 
-sudo apt install unzip 
+# Installing AWS CLI
+#!/bin/bash
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install

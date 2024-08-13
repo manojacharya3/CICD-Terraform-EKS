31af0f22980a0c5d22a7da0b0e542159b16a90fe
manojacharya3 <manojkumar.nandachar@perficient.com>
2024-08-13 20:12:24 +0530

    deleted

--- EKS/kubernetes.tf
+++ EKS/kubernetes.tf
@@ -4,32 +4,3 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
 
-resource "kubernetes_namespace" "test" {
-  metadata {
-    name = "nginx"
-  }
-}
-
-resource "kubernetes_deployment" "test" {
-  metadata {
-    name = "nginx"
-    namespace = kubernetes_namespace.test.metadata.0.name
-  }
-  spec{
-    replicas = 2
-    selector {
-      match_labels ={
-        app = "MyTestApp"
-    
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "MyTestApp"
-        }
-      }
-    }
-  }
-}
